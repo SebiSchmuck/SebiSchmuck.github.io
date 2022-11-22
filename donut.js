da5f3ec6cb7bda0c2a198d3301d5adf0e9eb8690
Sebastian Schmuck <sebastian.schmuck@stud.fh-rosenheim.de>
2022-11-23 00:30:29 +0100

    Code readability

--- donut.js
+++ donut.js
@@ -1,52 +1,60 @@
 var pretag = document.getElementById("d");
 
-var tmr1 = undefined,
-  tmr2 = undefined;
-var A = 1,
-  B = 1;
+var axisA = 1;
+var axisB = 1;
 
-var asciiframe = () => {
-  var b = [];
-  var z = [];
-  A += 0.07;
-  B += 0.03;
-  var cA = Math.cos(A),
-    sA = Math.sin(A),
-    cB = Math.cos(B),
-    sB = Math.sin(B);
+var thetaSpacing = 0.07;
+var phiSpacing = 0.02;
+
+var twoPI = 6.28;
+
+var asciiFrame = () => {
+  var output = [];
+  var zBuffer = [];
+
+  axisA += thetaSpacing;
+  axisB += phiSpacing;
+
+  var cosAxisA = Math.cos(axisA);
+  var sinAxisA = Math.sin(axisA);
+  var cosAxisB = Math.cos(axisB);
+  var sinAxisB = Math.sin(axisB);
+
   for (var k = 0; k < 1760; k++) {
-    b[k] = k % 80 == 79 ? "\n" : " ";
-    z[k] = 0;
+    output[k] = k % 80 == 79 ? "\n" : " ";
+    zBuffer[k] = 0;
   }
-  for (var j = 0; j < 6.28; j += 0.07) {
-    // j <=> theta
-    var ct = Math.cos(j),
-      st = Math.sin(j);
-    for (i = 0; i < 6.28; i += 0.02) {
-      // i <=> phi
-      var sp = Math.sin(i),
-        cp = Math.cos(i),
-        h = ct + 2, // R1 + R2*cos(theta)
-        D = 1 / (sp * h * sA + st * cA + 5), // this is 1/z
-        t = sp * h * cA - st * sA; // this is a clever factoring of some of the terms in x' and y'
 
-      var x = 0 | (40 + 30 * D * (cp * h * cB - t * sB)),
-        y = 0 | (12 + 15 * D * (cp * h * sB + t * cB)),
-        o = x + 80 * y,
-        N =
-          0 |
-          (8 *
-            ((st * sA - sp * ct * cA) * cB -
-              sp * ct * sA -
-              st * cA -
-              cp * ct * sB));
-      if (y < 22 && y >= 0 && x >= 0 && x < 79 && D > z[o]) {
-        z[o] = D;
-        b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+  for (var theta = 0; theta < twoPI; theta += thetaSpacing) {
+    var cosTheta = Math.cos(theta);
+    var sinTheta = Math.sin(theta);
+
+    for (var phi = 0; phi < twoPI; phi += phiSpacing) {
+      var sinPhi = Math.sin(phi);
+      var cosPhi = Math.cos(phi);
+
+      var circleX = cosTheta + 2; // R1 + R2 * cos(theta)
+      var circleY = sinPhi * circleX * cosAxisA - sinTheta * sinAxisA; // This is a clever factoring of some of the terms in x' and y'
+      var z_1 = 1 / (sinPhi * circleX * sinAxisA + sinTheta * cosAxisA + 5); // This is 1/z
+
+      var x = 0 | (40 + 30 * z_1 * (cosPhi * circleX * cosAxisB - circleY * sinAxisB));
+      var y = 0 | (12 + 15 * z_1 * (cosPhi * circleX * sinAxisB + circleY * cosAxisB));
+      var charIndex = x + 80 * y;
+      
+      var luminance = 0 | (8 *
+            ((sinTheta * sinAxisA - sinPhi * cosTheta * cosAxisA) * cosAxisB -
+              sinPhi * cosTheta * sinAxisA -
+              sinTheta * cosAxisA -
+              cosPhi * cosTheta * sinAxisB));
+
+      if (y < 22 && y >= 0 && x >= 0 && x < 79 && z_1 > zBuffer[charIndex]) {
+        zBuffer[charIndex] = z_1;
+        output[charIndex] = ".,-~:;=!*#$@"[luminance > 0 ? luminance : 0];
       }
     }
   }
-  pretag.innerHTML = b.join("");
+
+  pretag.innerHTML = output.join("");
 };
 
-setInterval(asciiframe, 50)
+setInterval(asciiFrame, 50)
